5e5da56128e7f59a30461b884883dd4d1781cb78
ThaliaRowan <thalia.rowan@gmail.com>
2021-01-02 00:13:52 -0400

    created react app and components

---
+++ myportfolio/src/components/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+function About() {
+  return (
+<div> 
+    <div className="container">
+        <div class="col col-lg-6">
+            <h2>ABOUT ME</h2>
+            <article>
+                Hi, My name is Thalia Romany.<br/>
+
+                I love building and creating new things. My interest in this field raised during highschool, where I began researhing more on the job titles<br />
+
+                I attended the Uconn coding bootcamp to begin my journey in learning the skills needed to achieve my goals.
+
+                You can reach me on: <br/>          
+
+                <a href="https://www.linkedin.com/in/thalia-romany-559b15183"> LinkedIn
+                </a><br/>     
+                <a href="https://github.com/ThaliaRowan"> Github</a><br/>
+                        
+            </article>
+        </div> 
+        <div class="col col-lg-6">
+        <img src="./assets/myPic (2).JPG" style="width: 20rem;" id="myPic" />      
+        </div>
+         
+  
+        
+    </div>
+</div>
+
+        
+  );
+}
+
+
+export default About;
