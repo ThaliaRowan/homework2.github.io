00bf24b2fbff6962b48996c1cc9696e9f36212f3
ThaliaRowan <thalia.rowan@gmail.com>
2021-01-02 15:47:10 -0400

    created Footer component

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App(){
     return (
@@ -13,6 +14,7 @@ function App(){
             <Route exact path="/about" component={About} />
             <Route exact path="/portfolio" component={Portfolio} />
             <Route exact path="/Contact" component={Contact} />
+            <Footer />
         </div>
         </Router>
        
