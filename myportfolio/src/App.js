02715e6587545857b1063688111994d12266040e
ThaliaRowan <thalia.rowan@gmail.com>
2021-01-03 01:33:23 -0400

    fixed readme

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -14,7 +14,7 @@ function App(){
              <div>
              
             <Header />
-            <Route exact path="/about" component={About} />
+            <Route exact path="/" component={About} />
             <Route exact path="/portfolio" component={Portfolio} />
             <Route exact path="/Contact" component={Contact} />
             <Footer />
